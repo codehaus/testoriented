d3e1920f7b3b62a0656a2ec506ae40a25933e4f1
olivier.dagenais <olivier.dagenais@1bba5ef2-fc51-0410-a39f-9368bb71aebf>
2006-12-10 14:41:29 +0000

    Overview: Now using the JDT's java search engine to track down an associated test class based on the convention of placing test classes in a sub-package called "test" and called the same as the class under test.
    Changes:
    1 - Implemented most of fetchAssociatedTestClass by delegating the actual searching to a new method called findClass.
    
    2 - Implemented findClass using the JDT SearchEngine class and related classes, based on the topic "Using the Java search engine" at http://help.eclipse.org/help30/index.jsp?topic=/org.eclipse.jdt.doc.isv/guide/jdt_api_search.htm
    
    3 - Implemented the SimpleSearchRequestor for use in searching.
    
    
    File operations:
    ADDED src/org/dyndns/opendemogroup/todd/SimpleSearchRequestor.java
    
    
    Manual tests:
    1 - Launched Eclipse, imported the first iteration of the ReallySimpleProject, invoked our action on a method and hit a breakpoint in findClass.  Stepped through and confirmed that no results were returned by the search because that version did not have an associated test class.
    
    2 - Repeated the first test but with the most recent ReallySimpleProject and this time the search found a single result; the associated test class.  Mission accomplished.

---
+++ src/org/dyndns/opendemogroup/todd/SimpleSearchRequestor.java
@@ -0,0 +1,33 @@
+package org.dyndns.opendemogroup.todd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.search.SearchMatch;
+import org.eclipse.jdt.core.search.SearchRequestor;
+
+/**
+ * A simple extension of {@link SearchRequestor} that collects all searches in
+ * a {@link List<SearchMatch>}, which can be obtained with 
+ * {@link #getResults()}.
+ */
+public class SimpleSearchRequestor extends SearchRequestor {
+
+	protected List<SearchMatch> results = new ArrayList<SearchMatch>();
+
+	/**
+	 * @see org.eclipse.jdt.core.search.SearchRequestor#acceptSearchMatch(org.eclipse.jdt.core.search.SearchMatch)
+	 */
+	@Override
+	public void acceptSearchMatch(SearchMatch match) throws CoreException {
+		// TODO Auto-generated method stub
+		results.add(match);
+	}
+
+	public List<SearchMatch> getResults() {
+		return results;
+	}
+
+	
+}
