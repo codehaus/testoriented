460e72081a30976a4141e4b4aa06dd0343688655
olivier.dagenais <olivier.dagenais@1bba5ef2-fc51-0410-a39f-9368bb71aebf>
2006-12-10 14:36:33 +0000

    Overview: Based on the article by Dan Kehn, implemented a general outline for the "Generate test(s)..." action where it starts peeking at the selection and sorting through it.
    Changes:
    1 - GenerateTestsAction.selectionChanged now collects some IMember instances if they are detected and sets the enabledness of the action accordingly.
    
    2 - GenerateTestsAction.run now checks the new _Members list (of IMember instances) and enumerates it for future processing.
    
    3 - Wrote lots of TODOs for future steps.
    
    
    Manual tests:
    1 - Set some breakpoints and launched a clean workspace in which some Java project was imported and then I tried to invoke the "Generate test(s)..." on various objects.  When I couldn't hit a breakpoint, I left a TODO.

--- trunk/src/org/dyndns/opendemogroup/todd/ui/actions/GenerateTestsAction.java
+++ trunk/src/org/dyndns/opendemogroup/todd/ui/actions/GenerateTestsAction.java
@@ -1,21 +1,26 @@
 package org.dyndns.opendemogroup.todd.ui.actions;
 
+import java.util.List;
+
+import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
 public class GenerateTestsAction implements IObjectActionDelegate {
 
 	/**
-	 * Constructor for Action1.
+	 * Target {@link IMember} instances to operate on.  In our case, it will be
+	 * methods ({@link IMethod}) and classes ({@link IType}). 
 	 */
-	public GenerateTestsAction() {
-		super();
-	}
-
+	List<IMember> _Members = null;
+	
 	/**
 	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
 	 */
@@ -26,17 +31,65 @@ public class GenerateTestsAction implements IObjectActionDelegate {
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction action) {
-		Shell shell = new Shell();
-		MessageDialog.openInformation(
-			shell,
-			"Todd Plug-in",
-			"Generate test(s)... was executed.");
+		if ( _Members != null ) {
+			for ( IMember each : _Members ) {
+				if (each instanceof IMethod) {
+					IMethod eachMethod = (IMethod) each;
+					// TODO: move this test to selectionChanged if possible
+					int flags = 0;
+					try {
+						flags = eachMethod.getFlags ( );
+					}
+					catch ( JavaModelException jme ) {
+						// TODO: Find out when this is thrown and see if there's
+						// a better reaction than simply stopping everything.
+						System.out.print ( "    -> JME thrown: " );
+						System.out.println ( jme );
+						return;
+					}
+					if ( (flags & Flags.AccPrivate) != 0 ) {
+						// can't call private member!
+					}
+					else if ( (flags & Flags.AccProtected) != 0) {
+						// TODO: Also consider an abstract method
+						// TODO: write a test for method only if test class subclasses us
+					}
+					else {
+						// TODO: write a test for method
+					}
+				}
+				else if (each instanceof IType) {
+					// TODO: I can't seem to trigger on IType and instead I get
+					// an instance of File when I think I'm on a Class...
+					IType eachClass = (IType) each;
+					// TODO: determine if eachClass is abstract and react accordingly
+					// TODO: write a testcase class
+					// TODO: Consider invoking the current "New JUnit Test Case" wizard
+				}
+			}
+		}
 	}
 
 	/**
+	 * Records the current selection for possible future processing using
+	 * {@link #run(IAction)}.
+	 * 
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO: Handle non-outline (JavaEditor) invocations
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection ss = (IStructuredSelection) selection;
+			_Members = ss.toList();
+		}
+		else {
+			_Members = null;
+			// TODO: Remove this once we no longer need it
+			System.out.print ( "    -> Selection type not yet handled: " );
+			System.out.println ( selection );
+		}
+		
+		action.setEnabled( _Members != null );
 	}
 
 }
