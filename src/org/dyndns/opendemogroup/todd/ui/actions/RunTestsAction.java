cd067bcfa51fe39c67365e820e2c07e70d5e1ab7
olivier.dagenais <olivier.dagenais@1bba5ef2-fc51-0410-a39f-9368bb71aebf>
2006-12-10 15:00:13 +0000

    Overview: Restructured the plugin's contributions to be exposed through a TODD sub-menu and added action skeletons for running and debugging tests.
    Changes:
    1 - Added a copy of a <menu> element to all contributions.
    
    2 - Changed all existing actions' @menubarPath attribute from "additions" to "todd.menu/group".
    
    3 - Added actions for RunTestsAction and DebugTestsAction to all contributions.
    
    4 - Created <command> extensions for each of the actions and linked all actions back to the new commands through the @definitionId attribute.
    
    5 - Added stub classes for the new actions.
    
    
    File operations:
    ADDED src/org/dyndns/opendemogroup/todd/ui/actions/DebugTestsAction.java
    ADDED src/org/dyndns/opendemogroup/todd/ui/actions/RunTestsAction.java
    
    
    Manual tests:
    Launched Eclipse on ReallySimpleProject and right-clicked on the usual targets.  The TODD sub-menu appeared, as did the three actions under it.  Made sure the Generate Test(s) action still worked and set breakpoints in the Run Test(s) and Debug Test(s) actions to make sure they could also be invoked.  Mission accomplished.

---
+++ src/org/dyndns/opendemogroup/todd/ui/actions/RunTestsAction.java
@@ -0,0 +1,45 @@
+package org.dyndns.opendemogroup.todd.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class RunTestsAction 
+	implements IObjectActionDelegate, IEditorActionDelegate {
+
+	/**
+	 * @see org.eclipse.ui.IEditorActionDelegate#setActiveEditor(org.eclipse.jface.action.IAction, org.eclipse.ui.IEditorPart)
+	 */
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		// TODO Auto-generated method stub
+	
+	}
+
+	/**
+	 * @see org.eclipse.ui.IObjectActionDelegate#setActivePart(org.eclipse.jface.action.IAction, org.eclipse.ui.IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+	
+	}
+
+	/**
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
