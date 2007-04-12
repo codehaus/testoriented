09e808dbb8db0daf6903551a2fa8113f55758017
olivier.dagenais <olivier.dagenais@1bba5ef2-fc51-0410-a39f-9368bb71aebf>
2007-04-12 02:32:49 +0000

    Overview: Still more preparatory work for implementing smart test stub generation.
    Changes:
    1 - (incidental) Noticed that the Flags class had some handy static methods for determining if a method is private or protected and so replaced some bit-twiddling with explicit and clearer method calls.
    
    2 - In preparation for smarter test stub generation, increasing the number of parameters to GenerateTestsAction.generateTestMethod to include an IType representing the class in which the new test method is to be added.
    
    3 - In preparation for smart test stub generation, separating the test method body from the test method signature/container.  The body will eventually be generated in one or more external methods and inserted somewhere in the rest.  This separation should also make it easier to implement issue 2.
    
    4 - Updated unit test to match change #3.
    
    5 - In preparation for smarter test stub generation, extracted the TestingMethod helper class to its own compilation unit (which dragged with it a bunch of imports).  Aside from making the class public, there were no other changes.
    
    6 - (incidental) Extracted the local newLine variable in the unit test as a constant since I have a feeling it will be useful in more tests coming up real soon now.  Also renamed another local variable for clarity.
    
    
    File operations:
    ADD src/org/dyndns/opendemogroup/todd/ui/actions/TestingMethod.java
    
    
    Issue: 3

---
+++ branches/smart-templates/src/org/dyndns/opendemogroup/todd/ui/actions/TestingMethod.java
@@ -0,0 +1,239 @@
+package org.dyndns.opendemogroup.todd.ui.actions;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.jdt.core.IClassFile;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaModel;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IOpenable;
+import org.eclipse.jdt.core.ISourceRange;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.ITypeParameter;
+import org.eclipse.jdt.core.JavaModelException;
+
+public class TestingMethod implements IMethod {
+
+	private String _ElementName;
+
+	public String getElementName() {
+		return _ElementName;
+	}
+
+	public void setElementName(String elementName) {
+		_ElementName = elementName;
+	}
+
+	public String[] getExceptionTypes() throws JavaModelException {
+		return null;
+	}
+
+	public String getKey() {
+		return null;
+	}
+
+	public int getNumberOfParameters() {
+		// TODO: Implement this later
+		return 0;
+	}
+
+	public String[] getParameterNames() throws JavaModelException {
+		// TODO: Implement this later
+		return null;
+	}
+
+	public String[] getParameterTypes() {
+		// TODO: Implement this later
+		return null;
+	}
+
+	public String[] getRawParameterNames() throws JavaModelException {
+		return null;
+	}
+
+	public String getReturnType() throws JavaModelException {
+		// TODO: Implement this later
+		return null;
+	}
+
+	public String getSignature() throws JavaModelException {
+		return null;
+	}
+
+	public ITypeParameter getTypeParameter(String name) {
+		return null;
+	}
+
+	public String[] getTypeParameterSignatures() throws JavaModelException {
+		return null;
+	}
+
+	public ITypeParameter[] getTypeParameters() throws JavaModelException {
+		return null;
+	}
+
+	public boolean isConstructor() throws JavaModelException {
+		return false;
+	}
+
+	public boolean isMainMethod() throws JavaModelException {
+		return false;
+	}
+
+	public boolean isResolved() {
+		return false;
+	}
+
+	public boolean isSimilar(IMethod method) {
+		return false;
+	}
+
+	public String[] getCategories() throws JavaModelException {
+		return null;
+	}
+
+	public IClassFile getClassFile() {
+		return null;
+	}
+
+	public ICompilationUnit getCompilationUnit() {
+		return null;
+	}
+
+	public IType getDeclaringType() {
+		return null;
+	}
+
+	public int getFlags() throws JavaModelException {
+		// TODO: Implement this later
+		return 0;
+	}
+
+	public ISourceRange getJavadocRange() throws JavaModelException {
+		return null;
+	}
+
+	public ISourceRange getNameRange() throws JavaModelException {
+		return null;
+	}
+
+	public int getOccurrenceCount() {
+		return 0;
+	}
+
+	public IType getType(String name, int occurrenceCount) {
+		return null;
+	}
+
+	public boolean isBinary() {
+		return false;
+	}
+
+	public boolean exists() {
+		return true;
+	}
+
+	public IJavaElement getAncestor(int ancestorType) {
+		return null;
+	}
+
+	public String getAttachedJavadoc(IProgressMonitor monitor) throws JavaModelException {
+		return null;
+	}
+
+	public IResource getCorrespondingResource() throws JavaModelException {
+		return null;
+	}
+
+	public int getElementType() {
+		return IJavaElement.METHOD;
+	}
+
+	public String getHandleIdentifier() {
+		return null;
+	}
+
+	public IJavaModel getJavaModel() {
+		return null;
+	}
+
+	public IJavaProject getJavaProject() {
+		return null;
+	}
+
+	public IOpenable getOpenable() {
+		return null;
+	}
+
+	public IJavaElement getParent() {
+		return null;
+	}
+
+	public IPath getPath() {
+		return null;
+	}
+
+	public IJavaElement getPrimaryElement() {
+		return null;
+	}
+
+	public IResource getResource() {
+		return null;
+	}
+
+	public ISchedulingRule getSchedulingRule() {
+		return null;
+	}
+
+	public IResource getUnderlyingResource() throws JavaModelException {
+		return null;
+	}
+
+	public boolean isReadOnly() {
+		return false;
+	}
+
+	public boolean isStructureKnown() throws JavaModelException {
+		return true;
+	}
+
+	public Object getAdapter(Class adapter) {
+		return null;
+	}
+
+	public String getSource() throws JavaModelException {
+		return null;
+	}
+
+	public ISourceRange getSourceRange() throws JavaModelException {
+		return null;
+	}
+
+	public void copy(IJavaElement container, IJavaElement sibling, String rename, boolean replace, IProgressMonitor monitor) throws JavaModelException {
+		
+	}
+
+	public void delete(boolean force, IProgressMonitor monitor) throws JavaModelException {
+		
+	}
+
+	public void move(IJavaElement container, IJavaElement sibling, String rename, boolean replace, IProgressMonitor monitor) throws JavaModelException {
+	
+	}
+
+	public void rename(String name, boolean replace, IProgressMonitor monitor) throws JavaModelException {
+		
+	}
+
+	public IJavaElement[] getChildren() throws JavaModelException {
+		return null;
+	}
+
+	public boolean hasChildren() throws JavaModelException {
+		return false;
+	}
+}
